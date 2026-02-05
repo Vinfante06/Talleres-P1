d12653a3ad9c12710fcca39594c3240f8b7b277c
VDINFANTE <vdinfanteg@gmail.com>
2026-02-05 11:40:11 -0500

    cambios en la funcion home

--- movie/views.py
+++ movie/views.py
@@ -4,7 +4,7 @@ from django.http import HttpResponse
 def home(request):
     #return HttpResponse('<h1>Welcome to the Movie Reviews Project!</h1>')
     #return render(request, 'home.html')
-    return render(request, 'home.html' , {'name': 'Grem Lim'})
+    return render(request, 'home.html' , {'name': 'Victor Infante'})
 
 def about(request):
     return HttpResponse("This is the about page.")
