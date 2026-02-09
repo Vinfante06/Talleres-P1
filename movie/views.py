10649c7f61dbee52e0325dffb5ebbf6f9d91a333
VDINFANTE <vdinfanteg@gmail.com>
2026-02-08 20:31:37 -0500

    visualización y busqueda

--- movie/views.py
+++ movie/views.py
@@ -1,10 +1,22 @@
 from django.shortcuts import render 
 from django.http import HttpResponse
 
+from models import Movie
+
+
 def home(request):
     #return HttpResponse('<h1>Welcome to the Movie Reviews Project!</h1>')
     #return render(request, 'home.html')
-    return render(request, 'home.html' , {'name': 'Victor Infante'})
+    #return render(request, 'home.html' , {'name': 'Victor Infante'})
+    searchterm = request.GET.get('searMovie')
+    movies = Movie.objects.all()
+    searchTerm = request.GET.get('searchMovie')
+    if searchTerm:
+        movies = Movie.objects.filter(title_icontains=searchTerm)
+    else: 
+        movies = Movie.object.all()
+    return render(request, 'home.html', {'searchTerm': searchterm, 'movies': movies})
+
 
 def about(request):
     return render(request, 'about.html')
