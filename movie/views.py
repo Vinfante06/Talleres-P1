77fc2cf47ffe8e15d195b150524fa67225aa1e95
VDINFANTE <vdinfanteg@gmail.com>
2026-02-08 21:33:36 -0500

    busqueda de peliculas

--- movie/views.py
+++ movie/views.py
@@ -8,16 +8,15 @@ def home(request):
     #return HttpResponse('<h1>Welcome to the Movie Reviews Project!</h1>')
     #return render(request, 'home.html')
     #return render(request, 'home.html' , {'name': 'Victor Infante'})
-    searchTerm = request.GET.get('searMovie')
-    movie = Movie.objects.all()
+    searchTerm = request.GET.get('searchMovie')
+    movies = Movie.objects.all()
     searchTerm = request.GET.get('searchMovie')
     if searchTerm:
-        movie = Movie.objects.filter(title__icontains=searchTerm)
+        movies = Movie.objects.filter(title__icontains=searchTerm)
     else: 
-        movie = Movie.objects.all()
-    return render(request, 'home.html', {'searchTerm': searchTerm, 'movie': movie})
+        movies = Movie.objects.all()
+    return render(request, 'home.html', {'searchTerm': searchTerm, 'movies': movies})
 
 
 def about(request):
     return render(request, 'about.html')
-
